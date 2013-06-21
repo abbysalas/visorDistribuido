8a35e4e44e9c65ad23b153e33c068d116f245f51
Alberto <alberto@MacBook-Air-de-Alberto.local>
2013-06-21 14:47:00 -0500

    Prueba

---
+++ visorDistribuido/visorDistribuido/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  visorDistribuido
+//
+//  Created by Alberto on 21/06/13.
+//  Copyright (c) 2013 Alberto. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ visorDistribuido/visorDistribuido/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  visorDistribuido
+//
+//  Created by Alberto on 21/06/13.
+//  Copyright (c) 2013 Alberto. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
