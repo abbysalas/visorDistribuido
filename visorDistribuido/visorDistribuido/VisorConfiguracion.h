be721f1c6629e4407a2bc1bb478ecd340d343658
Alberto <alberto@MacBook-Air-de-Alberto.local>
2013-06-21 15:17:47 -0500

    Agregue clase  Visor configuracion

---
+++ visorDistribuido/visorDistribuido/VisorConfiguracion.h
@@ -0,0 +1,13 @@
+//
+//  VisorConfiguracion.h
+//  visorDistribuido
+//
+//  Created by Alberto on 21/06/13.
+//  Copyright (c) 2013 Alberto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VisorConfiguracion : NSObject
+
+@end
---
+++ visorDistribuido/visorDistribuido/VisorConfiguracion.m
@@ -0,0 +1,13 @@
+//
+//  VisorConfiguracion.m
+//  visorDistribuido
+//
+//  Created by Alberto on 21/06/13.
+//  Copyright (c) 2013 Alberto. All rights reserved.
+//
+
+#import "VisorConfiguracion.h"
+
+@implementation VisorConfiguracion
+
+@end
